77402a38102a202938372ec274d6be00530bf4c7
Abigael-Wasabi <94919325+Abigael-Wasabi@users.noreply.github.com>
2022-03-20 12:34:13 +0300

    added file sum.py
    contains samples of functions

---
+++ sum.py
@@ -0,0 +1,11 @@
+def sum():
+    for i in range(1, 30):
+        if i % 3 == 0:
+            print(i)
+        if i % 5 == 0:
+            print(i)
+            summation = i
+        print(summation)
+
+
+sum()
